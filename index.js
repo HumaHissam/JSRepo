e38f52eddb8d8703b489194d57cb7b92f6445814
Humera Shah <humahissam@outlook.com>
2022-02-01 23:20:56 -0700

    Java Script week one

---
+++ index.js
@@ -0,0 +1,12 @@
+function askName(){
+    var name = prompt("Please enter your name.");
+   // alert("Hi " + name + ". Thankyou for visiting my website!");
+
+     displayName(name);
+   
+}
+
+function displayName(name) {
+  var name
+    alert("Hi " + name + ". Thankyou for visiting my website!");
+}
