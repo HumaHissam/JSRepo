289f8e216a9f8d59d15a7ff09a615a1d7f4612b6
Humera Shah <humahissam@outlook.com>
2022-02-10 21:06:12 -0700

    fixed the time-conversion-into-12-hour issue

--- index.js
+++ index.js
@@ -10,33 +10,36 @@ function askName(){
 function displayName(name) {
   var name
    // alert("Hi " + name + ". Thankyou for visiting my website! " + cDay + cMonth + cYear );
-   let currentDate = new Date();
-   let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+  let currentDate = new Date();
+  let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
   
-       let currentHour = currentDate.getHours();
+  let currentHour = currentDate.getHours();
+      
+  let hour = null ; 
+        if (currentHour == 0){
+                              hour = 12 ; 
+                             // alert("Good Morning " + name + ". It is " + hour + ":" + currentDate.getMinutes() + " AM. Thankyou for visiting my website!");
+        }
+        else if (currentHour > 0 && currentHour < 12){
+                              hour = currentHour;
+                             // alert("Good Morning " + name + ". It is " + hour + ":" + currentDate.getMinutes() + " AM. Thankyou for visiting my website!");
+        }
+        else if (currentHour == 12){
+                              hour = currentHour;
+                              //alert("Good AfterNoon " + name + ". It is " + hour + ":" + currentDate.getMinutes() + " PM. Thankyou for visiting my website!");
+        }
+    
+        else if (currentHour > 12 && currentHour <= 23){
+                              hour = currentHour - 12;
+                             // alert("Good Evening " + name + ". It is " + hour + ":" + currentDate.getMinutes() + " PM. Thankyou for visiting my website!");
+        }
 
-       
-       //  let hour ;
-      //  if (currentHour == 0){
-      //    hour = 12 ;
-      //    alert("Good Morning " + name + ". It is " + hour + "AM. Thankyou for visiting my website!");
-      //  } else if (currentHour > 0 && currentHour < 12){
-      //    hour = currentHour;
-      //    alert("Good Morning " + name + ". It is " + hour + "AM. Thankyou for visiting my website!");
-      //  }else if (currentHour = 12){
-      //    hour = currentHour;
-      //    alert("Good AfterNoon " + name + ". It is " + hour + "PM. Thankyou for visiting my website!");
-      //  }else if (currentHour > 12 && currentHour <= 23){
-      //    hour = currentHour - 12;
-      //    alert("Good Evening " + name + ". It is " + hour + "PM. Thankyou for visiting my website!");
-      //  }
-
-   if (currentHour >=5 && currentHour <= 12){
-      alert("Good Morning " + name + ". It is " + currentHour + ":"  + currentDate.getMinutes() +" hours. Thankyou for visiting my website!");
-} else if (currentHour > 12 && currentHour <= 18){
-  alert("Good Afternoon " + name + ". It is " + currentHour + ":"  + currentDate.getMinutes() +" hours. Thankyou for visiting my website!");
-} else if (currentHour > 18){
-  alert("Good Evening " + name + ". It is " + currentHour + ":"  + currentDate.getMinutes() +" hours. Thankyou for visiting my website!");
-}
+    if (currentHour >=5 && currentHour <= 12){
+alert("Good Morning " + name + ". It is " + hour + ":" + currentDate.getMinutes() + " AM. Thankyou for visiting my website!");
+ } else if (currentHour >= 12 && currentHour <= 18){
+  alert("Good Afternoon " + name + ". It is " + hour + ":" + currentDate.getMinutes() + " PM. Thankyou for visiting my website!");
+ } else if (currentHour > 18){
+  alert("Good Evening " + name + ". It is " + hour + ":" + currentDate.getMinutes() + " PM. Thankyou for visiting my website!");
+ }
 }
 
